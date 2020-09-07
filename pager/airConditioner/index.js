23cf450d36cbadf89c95ff265677305b98e8def3
Taya <1363892414@qq.com>
2020-09-07 11:11:48 +0800

    2020-9-7-11:11

--- pager/airConditioner/index.js
+++ pager/airConditioner/index.js
@@ -2,3 +2,18 @@ let iconback = document.getElementById('iconback');
 iconback.onclick = function(){
     window.history.go(-1);
 }
+
+let temp = document.getElementById('temp');
+
+function add(){
+    let temp_num = temp.innerHTML;
+    temp_num = temp_num.split('℃')[0];
+    temp_num = Number.parseInt(temp_num)+1;
+    temp.innerHTML = temp_num+'℃';
+}
+function sub(){
+    let temp_num = temp.innerHTML;
+    temp_num = temp_num.split('℃')[0];
+    temp_num = Number.parseInt(temp_num)-1;
+    temp.innerHTML = temp_num+'℃';
+}
