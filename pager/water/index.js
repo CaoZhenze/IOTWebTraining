e564d660ae9bf1e2da34bd58e178891c2d219efe
Taya <1363892414@qq.com>
2020-09-07 11:01:50 +0800

    2020-9-7-11:01

--- pager/water/index.js
+++ pager/water/index.js
@@ -1,6 +1,26 @@
 
 let btn_back = document.getElementsByClassName('btn_back')[0];
+let device_temp = document.getElementById('device_temp');
+let header = document.getElementsByClassName('header')[0];
+
+let color = ['#F3B73F', '#1E90FF', '#FF0000', '#FFC0CB', '#00FF00', '#9400D3']
 
 btn_back.onclick = function () {
     window.history.go(-1);
+}
+
+function btn_sub(){
+    let temp = device_temp.innerHTML;
+    temp = Number.parseInt(temp)-1;
+    device_temp.innerHTML = temp;
+}
+
+function btn_add(){
+    let temp = device_temp.innerHTML;
+    temp = Number.parseInt(temp)+1;
+    device_temp.innerHTML = temp;
+}
+
+function change_state(index){
+    header.style.background = color[index];
 }
